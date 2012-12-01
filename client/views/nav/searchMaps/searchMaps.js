61634c1efe9e7adadd0336d63e0ba077f4cf58f5
Romain Vergnory <poltuu@gmail.com>
2012-12-01 20:54:22 +0100

    mod title to name

--- client/views/nav/searchMaps/searchMaps.js
+++ client/views/nav/searchMaps/searchMaps.js
@@ -4,7 +4,7 @@ Template.searchMaps.rendered = function(){
 	  Meteor.autorun(function () {
 		$('#map-search').typeahead({
 			source: Maps.find().fetch(),
-			tmpl: _.template('<li id="<%= _id %>"><a href="#"><%= name %></a></li>')
+			tmpl: _.template('<li id="<%= _id %>"><a href="#"><%= title %></a></li>')
 
 		});	
 
