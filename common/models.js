e08a0fa81416284a79da0854907117df4832f2da
Xavier <x.campenon@gmail.com>
2012-12-01 18:48:26 +0100

    creations de points

--- common/models.js
+++ common/models.js
@@ -1,3 +1,18 @@
 Maps = new Meteor.Collection("maps");
 
 Filters = new Meteor.Collection("filters");
+
+function point (latlng, title, image) {
+		if(latlng==null) {
+			latlng = new google.maps.LatLng(-25.363882,131.044922);
+		}
+		if(title==null) {
+			title = "titre test";
+		}
+		if(image==null) {
+			image = 'glyphicons-halflings.png';
+		}
+
+	return {latlng: latlng, title: title, image: }
+
+}
