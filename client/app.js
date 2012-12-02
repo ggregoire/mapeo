113c590937bd97f3e4551caf1530ea341db87bdd
ggregoire <guillaume.gregoire.89@gmail.com>
2012-12-02 02:12:43 +0100

    url map courrante dans l'url

--- client/app.js
+++ client/app.js
@@ -38,4 +38,26 @@ Meteor.startup(function () {
 		tmpl: _.template('<li id="<%= id %>"><img src="<%= img %>" width="32" height="32" /><a href="#"><%= name %></a></li>')
 	});
 
+	var loadMap = function (id) {
+		if (Maps.findOne(id)) {
+			Session.set('selectedMap', id);
+		}
+	};
+
+	if (window.location.hash) {
+		loadMap(window.location.hash.replace(/#/, ''));
+	} else {
+		Meteor.autorun(function (handle) {
+			if (!Maps.findOne()) {
+		    	return;
+		    }
+		    handle.stop();
+			window.location.hash = '#' + Session.get('selectedMap');
+		});
+	}
+
+	$(window).bind('hashchange', function () {
+		loadMap(window.location.hash.replace(/#/, ''));
+	});
+
 });
